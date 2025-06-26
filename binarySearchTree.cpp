c0a094e30718c8610a18a24412b0dbec7dba8715
RifalArdi25 <rifalardi25@gmail.com>
2025-06-26 11:11:46 +0700

    Membuat traversal postorder

--- binarySearchTree.cpp
+++ binarySearchTree.cpp
@@ -126,6 +126,25 @@ class BinaryTree
         preorder(ptr->rightchild);
     }
 
+    void postorder(Node *ptr)
+    {
+        if (isEmpty())
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr == nullptr)
+            return;
+            
+        postorder(ptr->leftchild);
+        postorder(ptr->rightchild);
+        cout << ptr->info << " "; // parent        
+    }
 
+    bool isEmpty()
+    {
+        // Checks if the tree is empty
+        return ROOT == nullptr;
+    }
 };
 
