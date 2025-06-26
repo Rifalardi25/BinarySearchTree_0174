d671d7660b14ea8728ea530850daa0f0e3ae3021
RifalArdi25 <rifalardi25@gmail.com>
2025-06-26 11:00:42 +0700

    Membuat langkah-langkah insert

--- binarySearchTree.cpp
+++ binarySearchTree.cpp
@@ -31,8 +31,58 @@ class BinaryTree
         cout << "Masukkan Nilai: ";
         cin >> x;
 
+        // Step 1: Allocate memory for new node
+        Node *newNode = new Node();
 
+        // Step 2: Assign value to the data field of the new node
+        newNode->info = x;
+
+        // Step 3: Make the left and right child of the new node point to NULL
+
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
+
+        // Step 4: Locate the node which will be the parent of the node to be inserted
+        Node *parent = nullptr;
+        Node *currentNode = nullptr;
+        search (x, parent, currentNode);
+
+        // Step 5: If parent is NULL (Tree is empty)
+        if (parent == nullptr)
+        {
+            // 5a. Mark the new node as ROOT
+            ROOT = newNode;
+
+            // 5b. Exit
+
+            return;
+        }
+
+
+        // Step 6: If the value in the data field of new node is less then that of parent
+        if (x < parent->info)
+        {
+            // 6a. Make the left child of parent point to the new node
+            parent->leftchild = newNode;
+
+
+            // 6b. Exit
+            return;
+        }
+
+        // Step 7: If the value in the data field of new node greater then that of the parent
+        else if (x > parent->info)
+        {
+            // 7a. Make the right child of parent point ti the new node
+            parent->rightchild = newNode;
+
+            // 7b. Exit
+            return;
+        } 
     }
 
 
+
+
 };
+
